505976ba2f4269be7641fcf45d29990505f2851f
David Khuu <davidkhuu.dk@gmail.com>
2019-06-02 10:52:42 -0600

    Set up login form

--- src/login/login.js
+++ src/login/login.js
@@ -1,13 +1,94 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import Paper from '@material-ui/core/Paper';
+import withStyles from '@material-ui/core/styles/withStyles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import styles from './styles';
+
+const firebase = require('firebase');
 
 class LoginComponent extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {
+      email: null,
+      password: null,
+      loginError: ''
+    };
+  }
+
+  userTyping = (type, e) => {
+    switch (type) {
+      case 'email':
+        this.setState({ email: e.target.value });
+        break;
+
+      case 'password':
+        this.setState({ password: e.target.value });
+        break;
+    
+      default:
+        break;
+    }
+  }
+
+  submitLogin = (e) => {
+    e.preventDefault();
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(this.state.email, this.state.password)
+      .then(() => {
+        this.props.history.push('/dashboard');
+      }, err => {
+        console.log(err);
+        this.setState({ loginError: 'Server error' });
+      });
+  }
+
   render() {
+    
+    const { classes } = this.props;
+
     return (
-      <div>Hello world from LoginComponent</div>
+      <main className={classes.main}>
+        <CssBaseline></CssBaseline>
+        <Paper className={classes.paper}>
+          <Typography component="h1" variant="h5">
+            Log In
+          </Typography>
+          <form className={classes.form} onSubmit={(e) => this.submitLogin(e)}>
+            <FormControl required fullWidth margin="normal">
+              <InputLabel htmlFor="login-email-input">Enter your e-mail</InputLabel>
+              <Input autoComplete="email" autoFocus id="login-email-input" onChange={(e) => this.userTyping('email', e)}></Input>
+            </FormControl>
+            <FormControl required fullWidth margin="normal">
+              <InputLabel htmlFor="login-password-input">Enter your e-mail</InputLabel>
+              <Input type="password" autoComplete="email" id="login-password-input" onChange={(e) => this.userTyping('password', e)}></Input>
+            </FormControl>
+            <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>Log in</Button>
+          </form>
+          {
+            this.state.loginError ?
+            <Typography component="h5" variant="h6" className={classes.errorText}>
+              Incorrect login information
+            </Typography> :
+            null
+          }
+          <Typography component="h5" variant="h6" className={classes.noAccountHeader}>
+            Don't have an account?
+          </Typography>
+          <Link className={classes.signUpLink} to="/signup">Sign up</Link>
+        </Paper>
+      </main>
     );
   }
 
 }
 
-export default LoginComponent;
+export default withStyles(styles)(LoginComponent);
