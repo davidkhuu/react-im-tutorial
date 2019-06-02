766574772d8a4c1960bcb942b6ec47e78249ca88
David Khuu <davidkhuu.dk@gmail.com>
2019-06-02 11:36:30 -0600

    Create chat list to display chats for current user

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -1,10 +1,59 @@
 import React from 'react';
+import ChatListComponent from '../chatlist/chatlist';
+import styles from './styles';
+const firebase = require('firebase');
 
 class DashboardComponent extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {
+      selectedChat: null,
+      newChatFormVisible: false,
+      email: null,
+      chats: []
+    };
+  }
+
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(async _usr => {
+      if (!_usr) {
+        this.props.history.push('/login');
+      } else {
+        await firebase
+          .firestore()
+          .collection('chats')
+          .where('users', 'array-contains', _usr.email)
+          .onSnapshot(async res => {
+            const chats = res.docs.map(_doc => _doc.data());
+            await this.setState({
+              email: _usr.email,
+              chats: chats
+            });
+            console.log(chats)
+          });
+      }
+    });
+  }
+
+  selectChat = (chatIndex) => {
+    console.log('selected chat', chatIndex)
+  }
+
+  newChatBtnClicked = () => this.setState({ newChatFormVisible: true, selectedChat: null });
+
   render() {
     return (
-      <div>Hello world from DashboardComponent</div>
+      <div>
+        <ChatListComponent 
+          history={this.props.history} 
+          newChatBtnFn={this.newChatBtnClicked}
+          selectChatFn={this.selectChat}
+          chats={this.state.chats}
+          userEmail={this.state.email}
+          selectedChatIndex={this.state.selectedChat}
+        />
+      </div>
     );
   }
 
