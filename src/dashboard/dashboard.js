1182bac8449b01ef78b7062d308b6c0685cce325
David Khuu <davidkhuu.dk@gmail.com>
2019-06-08 15:01:14 -0600

    Add sign out button to bottom of dashboard

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ChatListComponent from '../chatlist/chatlist';
+import { Button, withStyles } from '@material-ui/core';
 import styles from './styles';
 const firebase = require('firebase');
 
@@ -42,7 +43,10 @@ class DashboardComponent extends React.Component {
 
   newChatBtnClicked = () => this.setState({ newChatFormVisible: true, selectedChat: null });
 
+  signOut = () => firebase.auth().signOut();
+
   render() {
+    const { classes} = this.props;
     return (
       <div>
         <ChatListComponent 
@@ -53,10 +57,16 @@ class DashboardComponent extends React.Component {
           userEmail={this.state.email}
           selectedChatIndex={this.state.selectedChat}
         />
+        <Button
+          className={classes.signOutBtn}
+          onClick={this.signOut}
+        >
+          Sign Out
+        </Button>
       </div>
     );
   }
 
 }
 
-export default DashboardComponent;
+export default withStyles(styles)(DashboardComponent);
